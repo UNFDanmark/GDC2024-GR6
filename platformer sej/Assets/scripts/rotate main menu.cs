88dee9ac1cd90b0ca350296684b597bcaa71ceca
Deltager <teknisk+GDC2024-GR6@game.unf.dk>
2024-07-17 20:29:27 +0200

    G

--- platformer sej/Assets/scripts/rotate main menu.cs
+++ platformer sej/Assets/scripts/rotate main menu.cs
@@ -14,6 +14,6 @@ public float RotationSpeed = 1;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,RotationSpeed,0);
+        transform.Rotate(RotationSpeed,0,0);
     }
 }
