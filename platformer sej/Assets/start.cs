88b4e97c617f13c7297f634a7bebd8b18d867f1f
Deltager <teknisk+GDC2024-GR6@game.unf.dk>
2024-07-17 10:56:37 +0200

    menu added!
    title

---
+++ platformer sej/Assets/start.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class start : MonoBehaviour
+{
+    [SerializeField] private string sunken = "test1";
+    
+    public void startbutton()
+    {
+        SceneManager.LoadScene("test1");
+    }
+}
